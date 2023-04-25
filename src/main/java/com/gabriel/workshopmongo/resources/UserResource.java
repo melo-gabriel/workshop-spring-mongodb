e70989e2cf6268b6626a36bca18de61aa0b975c9
Gabriel <silva.gabrielmelosantos@gmail.com>
2023-04-24 23:14:29 -0300

    conectado ao mongodb com repository e service

--- src/main/java/com/gabriel/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabriel/workshopmongo/resources/UserResource.java
@@ -1,27 +1,28 @@
 package com.gabriel.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gabriel.workshopmongo.domain.User;
+import com.gabriel.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
+	
 	@GetMapping
 	public ResponseEntity<List<User>>  findAll(){
-		User maria = new User("1", "maria silva", "maria@gmail.com");
-		User leticia = new User("2", "leticia daniela", "leticia@gmail.com");
-		User jorge = new User("3", "jorge bento", "jorge@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, leticia,jorge));
+		
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
